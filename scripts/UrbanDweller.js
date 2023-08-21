d5fcedfbedc94516d0175b370c625deef3e8767a
Daniel Bennett <danielpsalmbennett@yahoo.com>
2023-08-21 12:34:58 -0500

    yes

--- scripts/UrbanDweller.js
+++ scripts/UrbanDweller.js
@@ -17,12 +17,12 @@ export const LocationTypeChoices = async () => {
     const locations = await response.json()
 
     document.addEventListener("change", handleLocationChange)
-
-    let choicesHTML = "<h2> Which type of area do you live in?</h2>"
-    for (const location of locations) {
-        choicesHTML += `<input type = 'radio' name='location' value='${location.id}' /> ${location.label}`
-        
-    }
     
+    let choicesHTML = "<h2> Which type of area do you live in?</h2>"
+    const divStringArray = locations.map((location) => {
+    choicesHTML += `<input type = 'radio' name='location' value='${location.id}' /> ${location.label}`
+    
+})
+    choicesHTML += divStringArray.join("");
     return choicesHTML
 }
